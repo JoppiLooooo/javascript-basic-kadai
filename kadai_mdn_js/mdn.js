45603d6a575f46a3baed2848c57768ddeb083077
JoppiLooooo <jr19981102@gmail.com>
2025-08-22 23:00:13 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,7 @@
 const today = new Date();
 
 const date1 = () => {
-    console.log(today.getFullYear() +"年"+ today.getMonth() +"月"+ today.getDay() +"日");
+    console.log(today.getFullYear() +"年"+ today.getMonth()+1 +"月"+ today.getDate() +"日");
   }
 
 date1();
